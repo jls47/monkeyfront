8843421e5466a9d5f8049b2123db9edfea4c35f1
jls47 <lukeschaefer@gmail.com>
2019-05-29 14:27:55 -0700

    frontpage has admin functionality

--- src/components/frontpage.vue
+++ src/components/frontpage.vue
@@ -2,26 +2,73 @@
 <div class="frontpage">
 	<h2>Recently added: </h2><br>
 	<div  v-for="song in recentSongs">
-		<h1>{{song.title}} - {{song.artist}}</h1><br>
+    <h1>
+    <a class="addbutton" 
+       v-if="adding == true && !(added.includes(song))"
+       v-on:click="addSong(song)">
+      <span class="mdi mdi-24px mdi-plus-circle-outline"></span>
+    </a>
+    <a class="removebutton"
+       v-else-if="adding == true && added.includes(song)"
+       v-on:click="removeSong(song)">
+      <span class="mdi mdi-24px mdi-minus-circle-outline"></span>
+    </a>
+    <a v-else>
+      <span class = "mdi mdi-12px mdi-microphone-variant"></span>
+    </a>
+		{{song.title}} - {{song.artist}}</h1><br>
 	</div>
 </div>
 </template>
 
 <script>
 import music from '@/services/requests';
+import store from '../main.js';
+import { mapActions, mapState } from 'vuex';
 export default {
   name: 'frontpage',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      recentSongs: []
+      recentSongs: [],
+      adding: false,
+      added: []
     }
+  },
+  methods: {
+    addSong(item){
+      console.log(item);
+      this.addItem(item);
+    },
+    removeSong(item){
+      this.removeItem(item);
+    },
+    ...mapActions([
+      'addItem',
+      'removeItem'
+    ])
   },
   mounted: function(){
   	music.getRecentSongs()
   		.then(data => {
   			this.recentSongs = data.data;
   		})
+  },
+  computed: {
+    select(){
+      this.adding = this.$store.getters.isSelect;
+    },
+    getAdded(){
+      this.added = this.$store.getters.getItems;
+    }
+  },
+  watch: {
+    select(){
+      this.adding = this.$store.getters.isSelect;
+    },
+    getAdded(){
+      this.added = this.$stores.getters.getItems;
+    }
   }
 }
 //Need media queries to make things less awkward
